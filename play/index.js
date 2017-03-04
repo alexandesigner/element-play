5b88ad6f3dd299a5ece9b1fef1ab0ccd3932581c
Allan Alexandre <alexandesigner@gmail.com>
2017-03-03 21:01:04 -0300

    setup element ui inside vue play

--- play/index.js
+++ play/index.js
@@ -1,8 +1,10 @@
 import { play } from 'vue-play'
-import MyButton from '../src/components/MyButton.vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-default/index.css';
+import MyButton from '../element/packages/button/src/button.vue'
 
 // Use `play` to describe component title
 // use .add to add scenario for that component
-play('MyButton')
+play('Button')
   .add('with text', h => h(MyButton, ['hello world']))
   .add('with emoji', h => h(MyButton, ['😃🍻']))
